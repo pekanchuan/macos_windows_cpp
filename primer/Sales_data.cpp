072673b1eaa9897007feb8d783dd95c49ca122e6
CHia <dozeuto@gmail.com>
2023-11-21 10:56:15 +0800

    update Sales_data

--- primer/Sales_data.cpp
+++ primer/Sales_data.cpp
@@ -2,25 +2,39 @@
 // Created by Chia on 2023/11/20.
 //
 
+#include <iostream>
+#include <string>
 #include "Sales_data.h"
 
-Sales_data& Sales_data::combine(const Sales_data&rhs) {
-    units_sold += rhs.units_sold;
-    revenue += rhs.revenue;
-    return *this;
-}
 
+int foo() {
+    Sales_data data1, data2;
 
-
-std::istream& read(std::istream&is, Sales_data&item) {
     double price = 0;
-    is >> item.bookNo >> item.units_sold >> price;
-    item.revenue = price * item.units_sold;
-    return is;
-}
 
-std::ostream& print(std::ostream&os, const Sales_data&item) {
-    os << item.isbn() << " " << item.units_sold << " "
-            << item.revenue << " " << item.avg_price();
-    return os;
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+    data1.revenue = data1.units_sold * price;
+
+    std::cin >> data2.bookNo >> data2.units_sold >> price;
+    data2.revenue = data2.units_sold * price;
+
+    if (data1.bookNo == data2.bookNo) {
+        unsigned totalCnt = data1.units_sold + data2.units_sold;
+        double totalRevenue = data1.revenue + data2.revenue;
+
+        std::cout << data1.bookNo << " " << totalCnt
+                << " " << totalRevenue << " ";
+        if (totalCnt != 0) {
+            std::cout << totalRevenue / totalCnt << std::endl;
+        }
+        else {
+            std::cout << "(no sales)" << std::endl;
+        }
+
+        return 0;
+    }
+    else {
+        std::cerr << "Data must refer to the same ISBN" << std::endl;
+        return -1;
+    }
 }
--- primer/Sales_data.h
+++ primer/Sales_data.h
@@ -5,35 +5,12 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 
-#include <iostream>
 #include <string>
 
 struct Sales_data {
-    Sales_data() = default;
-
-    Sales_data(const std::string&s): bookNo(s) {
-    }
-
-    Sales_data(const std::string&s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p * n) {
-    }
-
-    Sales_data(std::istream&);
-
-    std::string isbn() const { return bookNo; }
-
-    Sales_data& combine(const Sales_data&);
-
-    double avg_price() const { return revenue / units_sold; }
-
-
     std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+    unsigned units_sold;
+    double revenue;
 };
-
-std::istream& read(std::istream&is, Sales_data&item);
-
-std::ostream& print(std::ostream&os, const Sales_data&item);
-
 
 #endif //SALES_DATA_H
