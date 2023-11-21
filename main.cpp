f2bbccc34fcb63f0db2fcc3471476954c8673cae
CHia <dozeuto@gmail.com>
2023-11-21 17:15:38 +0800

    update main file

--- main.cpp
+++ main.cpp
@@ -1,40 +1,43 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+#include <cstddef>
+using std::size_t;
 
-#include "primer/exercises.h"
+#include <vector>
+using std::vector;
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
+using std::getline;
+
+#include <cctype>
+using std::isupper;
+using std::toupper;
+using std::islower;
+using std::tolower;
+using std::isalpha;
+using std::isspace;
+
 #include "primer/Sales_item.h"
 
-using namespace std;
-
 int main(int argc, char* argv[]) {
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+    for (vector<int>::const_iterator it = v.begin();
+         it != v.end();
+         ++it)
+        cout << *it << " ";
+    cout << endl;
 
-	Sales_item total;
+    for (auto it = v.begin(); it != v.end(); ++it)
+        cout << *it << ", ";
+    cout << endl;
 
-	if (cin >> total)
-	{
-		Sales_item trans;
-		while (cin >> trans)
-		{
-			if (total.isbn() == trans.isbn())
-			{
-				total += trans;
-			}
-			else
-			{
-				cout << total << endl;
-				total = trans;
-			}
-		}
-		cout << total << endl;
-	}
-	else
-	{
-		cerr << "No data?!" << endl;
-		return -1;
-	}
+    for (const auto&value: v)
+        cout << value << "-";
+    cout << endl;
 
-
-	return 0;
+    return 0;
 }
