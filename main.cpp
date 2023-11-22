5042419c945973acdcc7df9740de146773d05511
CHia <dozeuto@gmail.com>
2023-11-22 17:39:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <cstddef>
+
 using std::size_t;
 
 #include <vector>
+
 using std::vector;
 
 #include <iostream>
+
 using std::cin;
 using std::cout;
 using std::endl;
 
 #include <string>
+
 using std::string;
 using std::getline;
 
 #include <cctype>
+
 using std::isupper;
 using std::toupper;
 using std::islower;
@@ -21,17 +26,33 @@ using std::tolower;
 using std::isalpha;
 using std::isspace;
 
+#include <stdexcept>
+
+using std::runtime_error;
+
 #include "primer/Sales_item.h"
+#include "primer/Sales_data.h"
 
-int ia[] = { 0,1,2,3,4,5,6,7,8,9 };
 
-int main(int argc, char* argv[]) {
-	cout << sizeof(ia) << " " << sizeof(*ia) << endl;
-	constexpr size_t sz = sizeof(ia) / sizeof(*ia);
+int main(int argc, char *argv[]) {
+    vector<int> ivec;
+    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	int arr2[sz];
+    for (auto &r: v)
+        r *= 2;
 
-	cout << "ia size: " << sz << endl;
+    for (int r: v)
+        cout << r << " ";
+    cout << endl;
 
-	return 0;
+    char letter = 'A';
+    const char *p = &letter;
+//    *p = 'B';
+    p = nullptr;
+
+    char * const cp = &letter;
+    *cp = 'B';
+//    cp = nullptr;
+
+    return 0;
 }
