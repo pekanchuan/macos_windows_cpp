52fac42675b0a0ba81e0881f69bbba3be0c046e0
CHia <dozeuto@gmail.com>
2023-11-28 17:53:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,80 +1,21 @@
-#include <cstddef>
-
-using std::size_t;
-
 #include <vector>
 
 using std::vector;
+
+#include <iterator>
+
+using std::iterator;
 
 #include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::cerr;
-using std::istream;
-using std::ostream;
-
-#include <string>
-
-using std::string;
-using std::getline;
-
-#include <cctype>
-
-using std::isupper;
-using std::toupper;
-using std::islower;
-using std::tolower;
-using std::isalpha;
-using std::isspace;
-
-#include <stdexcept>
-
-using std::runtime_error;
-
-#include <initializer_list>
-
-using std::initializer_list;
-
-#include <sstream>
-
-using std::ostringstream;
-
-#include <iterator>
-
-using std::begin;
-using std::end;
-
-#include <fstream>
-
-using std::ifstream;
-
-#include <sstream>
-
-using std::ostringstream;
-using std::istringstream;
 
 #include <algorithm>
 
-using std::find;
-
-#include <list>
-
-using std::list;
-
-#include <forward_list>
-
-using std::forward_list;
-
-#include "primer/Sales_item.h"
-#include "primer/Sales_data.h"
-#include "primer/LocalMath.h"
-#include "primer/make_plural.h"
-#include "primer/Account.h"
-#include "primer/Debug.h"
-#include "primer/Account.h"
-#include "primer/Screen.h"
+using std::for_each;
+using std::transform;
 
 int main(int argc, char *argv[]) {
 
